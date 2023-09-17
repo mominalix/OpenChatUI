581c246febb1a594182c3cfce8ead6e495b07eb0
Momin Ali <43476254+mominalix@users.noreply.github.com>
2023-09-17 22:56:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import streamlit as st
 # Function to display user messages with rounded rectangle borders
 def user_message(message):
     st.markdown(f'<div class="user-message" style="display: flex; justify-content: flex-end; padding: 5px;">'
-                f'<div style="background-color: #196b1c; color: white; padding: 10px; border-radius: 10px; font-size:18px;">{message}</div>'
+                f'<div style="background-color: #196b1c; color: white; padding: 10px; border-radius: 10px; font-size:18px; margin-bottom:10px; margin-left:20px;">{message}</div>'
                 f'</div>', unsafe_allow_html=True)
 
 # Function to display bot messages with rounded rectangle borders
 def bot_message(message):
     st.markdown(f'<div class="bot-message" style="display: flex; padding: 5px;">'
-                f'<div style="background-color: #074c85; color: white; padding: 10px; border-radius: 10px; font-size:18px;">{message}</div>'
+                f'<div style="background-color: #074c85; color: white; padding: 10px; border-radius: 10px; font-size:18px; margin-bottom:10px; margin-right:20px;">{message}</div>'
                 f'</div>', unsafe_allow_html=True)
 
 # Define the main Streamlit app
